c378d70b209793731f8f2fa3397761195711d9ce
Dhaouadi Yassine <yassine.dhaouadi@insat.u-carthage.tn>
2022-12-19 00:46:08 +0100

    Ajoutez des fichiers projet.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TP4.Data;
+using TP4.Models;
+
+namespace TP4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            UniversityContext uContext = UniversityContext.GetContext();
+            List<Student> s = uContext.Student.ToList();
+            foreach (Student student in s)
+            {
+                Debug.WriteLine("****************************************");
+                Debug.WriteLine("ID : "+ student.id);
+                Debug.WriteLine("First Name : "+ student.first_name);
+                Debug.WriteLine("Last Name : "+ student.last_name);
+                Debug.WriteLine("Phone Number : "+ student.phone_number);
+                Debug.WriteLine("University : " + student.university);
+                Debug.WriteLine("Course : " + student.course);
+                Debug.WriteLine("TimeStamp : " + student.timestamp);
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            UniversityContext uContext = UniversityContext.GetContext();
+            return View();
+        }
+        public IActionResult Courses()
+        {
+            UniversityContext uContext = UniversityContext.GetContext();
+            StudentRepository StudentRepo = new StudentRepository(uContext);
+
+            List<string> courses = (List<string>)StudentRepo.GetCourses();
+
+            return View(courses);
+        }
+        [HttpGet]
+        [Route("/Course/{courseName}")]
+        public IActionResult CoursesStudents(string courseName)
+        {
+            UniversityContext uContext = UniversityContext.GetContext();
+            StudentRepository StudentRepo = new StudentRepository(uContext);
+
+            IEnumerable<Student> StudentsPerCourse = (IEnumerable<Student>)StudentRepo.GetStudentsPerCourse(courseName);
+            ViewBag.CourseName = courseName;
+
+            return View(StudentsPerCourse);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
---
+++ Data/UniversityContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using TP4.Models;
+
+namespace TP4.Data
+{
+    public class UniversityContext : DbContext
+    {
+        public DbSet<Student> Student { get; set; }
+
+        private static UniversityContext Context = null;
+
+        public static UniversityContext GetContext()
+        {
+            if(Context == null)
+            {
+                Context = Instantiate_UniversityContext();
+            }
+            return Context;
+        }
+
+        public UniversityContext(DbContextOptions o) : base(o) {}
+        public static UniversityContext Instantiate_UniversityContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<UniversityContext>();
+            optionsBuilder.UseSqlite("Data Source=C:\\Users\\Yassine\\source\\repos\\TP4\\db\\database.db");
+            Debug.WriteLine("********************************************** Entred in Instantiate_ **********************************************");
+            return new UniversityContext(optionsBuilder.Options);
+        }
+    }
+}
